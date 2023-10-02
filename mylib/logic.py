9b8040612c80b0ac00ced407f6d6dca404741e9e
jithsg <135303624+jithsg@users.noreply.github.com>
2023-10-02 18:33:05 +0000

    second cli added

--- mylib/logic.py
+++ mylib/logic.py
@@ -4,3 +4,7 @@ import wikipedia
 def wiki(name='War Goddess', length=1):
     """Return a summary from Wikipedia."""
     return wikipedia.summary(name, sentences=length)
+
+def search(name='War Goddess'):
+    """Return a list of Wikipedia search results."""
+    return wikipedia.search(name)
