2ce473ecf71b89927bfb57ddb5e5565ac56f150f
jithsg <135303624+jithsg@users.noreply.github.com>
2023-10-02 19:06:50 +0000

    uvicorn, fastapi added

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki as wiki_logic
+from mylib.logic import search as search_logic
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"Wikipedia API": "Welcome to the Wikipedia API"}
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Return a summary from Wikipedia."""
+    result = wiki_logic(name)
+    return {"result": result}
+
+@app.get("/search/{name}")
+async def search(name: str):
+    """Return a list of Wikipedia search results."""
+    result = search_logic(name)
+    return {"result": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
